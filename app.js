fd8e5e93a351ad0e36b3517701f141525ad16bc6
sandylee <wasd86910@gmail.com>
2021-05-17 20:31:34 +0800

    feat: modify new  page ui

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
