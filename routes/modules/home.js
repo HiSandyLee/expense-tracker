4cd470282a535698889ea1468d4ea0ca4e4a028d
sandylee <wasd86910@gmail.com>
2021-05-18 15:30:16 +0800

    feat: debug

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../category.json')
+console.log(Category)
 
 router.get('/', (req, res) => {
   Record.find()
@@ -10,7 +11,7 @@ router.get('/', (req, res) => {
     .then(records => {
       let totalAmount = 0
       records.forEach(record => {
-        record.icon = Category[record.category].icon;
+        record.icon = Category[record.category].icon
         totalAmount += record.amount
       })
       console.log(records)
